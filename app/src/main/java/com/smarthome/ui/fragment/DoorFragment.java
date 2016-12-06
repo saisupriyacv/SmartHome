ce88c3d381c38e531956bfbddf0763b9a7328c39
Naveen <shanwadnaveen@gmail.com>
2016-12-06 13:42:56 -0800

    Made changes to doorfragment for display purpose

--- app/src/main/java/com/smarthome/ui/fragment/DoorFragment.java
+++ app/src/main/java/com/smarthome/ui/fragment/DoorFragment.java
@@ -26,19 +26,18 @@ public class DoorFragment extends BaseFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstance){
 
         View view = inflater.inflate(R.layout.doorfragment, container, false);
-        text1 = (TextView) view.findViewById(R.id.FrontDoor);
-        ImageView img = (ImageView) view.findViewById(R.id.doorimage);
-        text2 = (TextView) view.findViewById(R.id.BackDoor);
-        text3 = (TextView) view.findViewById(R.id.SideDoor);
+       // text1 = (TextView) view.findViewById(R.id.FrontDoor);
+       // text2 = (TextView) view.findViewById(R.id.BackDoor);
+       // text3 = (TextView) view.findViewById(R.id.SideDoor);
         //Please add this in your Fragment in order to access doors
         Activity activity = getActivity();
         if(activity instanceof SecureMainActivity){
             SecureMainActivity myactivity = (SecureMainActivity) activity;
             obj = myactivity.GetObject();
         }
-        text1.setText(((SmartHomeStatus) obj).getState().getReported().getDoors().getFrontDoor());
-        text2.setText(((SmartHomeStatus) obj).getState().getReported().getDoors().getBackDoor());
-        text2.setText(((SmartHomeStatus) obj).getState().getReported().getDoors().getSideDoor());
+       // text1.setText(((SmartHomeStatus) obj).getState().getReported().getDoors().getFrontDoor());
+       // text2.setText(((SmartHomeStatus) obj).getState().getReported().getDoors().getBackDoor());
+       // text2.setText(((SmartHomeStatus) obj).getState().getReported().getDoors().getSideDoor());
 
         return view;
     }
@@ -48,9 +47,9 @@ public class DoorFragment extends BaseFragment {
 
         if (object instanceof SmartHomeStatus) {
             SmartHomeStatus smartHomeStatus = (SmartHomeStatus) object;
-            text1.setText(smartHomeStatus.getState().getReported().getDoors().getFrontDoor());
-            text2.setText(smartHomeStatus.getState().getReported().getDoors().getBackDoor());
-            text2.setText(smartHomeStatus.getState().getReported().getDoors().getSideDoor());
+           // text1.setText(smartHomeStatus.getState().getReported().getDoors().getFrontDoor());
+           // text2.setText(smartHomeStatus.getState().getReported().getDoors().getBackDoor());
+           // text2.setText(smartHomeStatus.getState().getReported().getDoors().getSideDoor());
         }
     }
 }

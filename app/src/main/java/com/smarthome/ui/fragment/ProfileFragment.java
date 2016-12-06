96218abc3d94b04f18f180ebb67e58b591257a2e
Naveen <shanwadnaveen@gmail.com>
2016-12-06 11:28:47 -0800

    Added animations to screen and added navigation drawer and profile,history fragments

---
+++ app/src/main/java/com/smarthome/ui/fragment/ProfileFragment.java
@@ -0,0 +1,24 @@
+package com.smarthome.ui.fragment;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.smarthome.R;
+
+public class ProfileFragment extends android.support.v4.app.Fragment {
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstance) {
+
+        View view = inflater.inflate(R.layout.profilefragment, container, false);
+
+
+        return view;
+    }
+
+}
